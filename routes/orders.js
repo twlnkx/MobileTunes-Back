be28eaac9e8e27533e216d6068c25aa8886f7f2a
k4l1br <150938268+k4l1br@users.noreply.github.com>
2024-04-03 03:21:05 +0800

    email

--- routes/orders.js
+++ routes/orders.js
@@ -2,6 +2,36 @@ const { Order } = require('../models/order');
 const express = require('express');
 const { OrderItem } = require('../models/order-item');
 const router = express.Router();
+const { Product } = require('../models/product');
+const { User } = require('../models/user');
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+//una
+var transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+        user: process.env.SMTP_EMAIL,
+        pass: process.env.SMTP_PASSWORD
+    }
+});
+
+// Function to send emails
+async function sendEmail(to, subject, text) {
+    try {
+        const info = await transporter.sendMail({
+            from: `"${process.env.SMTP_FROM_NAME}" <${process.env.SMTP_FROM_EMAIL}>`,
+            to: to,
+            subject: subject,
+            text: text
+        });
+        console.log('Email sent: ' + info.response);
+    } catch (error) {
+        console.error('Error sending email:', error);
+    }
+}
+
 
 router.get(`/`, async (req, res) => {
     const orderList = await Order.find().populate('user', 'name').sort({ 'dateOrdered': -1 });
@@ -9,7 +39,7 @@ router.get(`/`, async (req, res) => {
     if (!orderList) {
         res.status(500).json({ success: false })
     }
-   
+
     res.status(201).json(orderList)
 })
 
@@ -17,9 +47,9 @@ router.get(`/:id`, async (req, res) => {
     const order = await Order.findById(req.params.id)
         .populate('user', 'name')
         .populate({
-            path: 'orderItems', 
+            path: 'orderItems',
             populate: {
-                path: 'product', 
+                path: 'product',
                 populate: 'category'
             }
         });
@@ -31,29 +61,17 @@ router.get(`/:id`, async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-    const orderItemsIds = Promise.all(req.body.orderItems.map(async (orderItem) => {
-        console.log(req.body)
-        // const orderItemsIds = req.body.orderItems.map(async (orderItem) => {
+    try {
+        const orderItemsIds = await Promise.all(req.body.orderItems.map(async (orderItem) => {
             let newOrderItem = new OrderItem({
                 quantity: orderItem.quantity,
                 product: orderItem.product
-            })
-
+            });
             newOrderItem = await newOrderItem.save();
-
             return newOrderItem._id;
-        })
-    )
-        console.log(orderItemsIds)
-        const orderItemsIdsResolved =  await orderItemsIds;
-        console.log(orderItemsIds)
-        // const totalPrices = await Promise.all(orderItemsIdsResolved.map(async (orderItemId)=>{
-        //     const orderItem = await OrderItem.findById(orderItemId).populate('product', 'price');
-        //     const totalPrice = orderItem.product.price * orderItem.quantity;
-        //     return totalPrice
-        // }))
+        }));
 
-        // const totalPrice = totalPrices.reduce((a,b) => a +b , 0);
+        const orderItemsIdsResolved = await orderItemsIds;
 
         let order = new Order({
             orderItems: orderItemsIdsResolved,
@@ -64,16 +82,42 @@ router.post('/', async (req, res) => {
             country: req.body.country,
             phone: req.body.phone,
             status: req.body.status,
-            // totalPrice: totalPrice,
             user: req.body.user,
-        })
+        });
+
         order = await order.save();
+
+        // Retrieve orderItems from the order object
+        const orderItems = await OrderItem.find({ _id: { $in: order.orderItems } }).populate('product');
+
+        // Send email confirmation
+        const subject = 'EarTunes - Order Confirmation';
+        let text = `Dear Customer,\n
+                    \nif you are reading this, our mailtrap is working!\n- Elon Musk\n
+                    \nHave a great day ahead!\n`;
+
+        for (const item of orderItems) {
+
+            // text += `MOTOHUB PRODUCT\n\n Kyt Helmet\n`;
+        }
+
+        text += `\nIf you have any inquiries or issues, please don't hesitate to reach out to us!\n
+                \n-Eartunes`;
+
+        await sendEmail('customer@gmail.com', subject, text);
+
+        res.status(201).json(order);
 
         if (!order)
             return res.status(400).send('the order cannot be created!')
+    } catch (error) {
 
-        res.send(order);
-    })
+        console.error('Error creating order:', error);
+        res.status(500).json({ success: false, error: error.message });
+    }
+});
+
+
 
 
 
